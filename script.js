5b72606f60fc98edd26be0109cc4266643f2e092
Ben Smith <benlsmith329@gmail.com>
2022-04-12 20:54:12 -0400

    added function with if to display weather

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ var currentCityName;
 var APIkey = '23b89ccd37ecfa0524d0c35eae3690f8';
 var weather = [];
 var cityList = [];
+
+function displayWeather(weather) {
+    $("day0-wind").text(weather[0].wind);
+    $("#day0-UV").text(weather[0].UV);
+    $(`#currentCityname`).text(currentCityName);
+    if (weather[0].UV >= 11) {varUV = `Violet`}; 
+    if (weather[0].UV < 11) {varUV = `Red`};
+    if (weather[0].UV < 8) {varUV = `Orange`};
+    if (weather[0].UV < 6) {varUV = `Yellow`};
+    if (weather[0].UV < 3) {varUV = `Green`};
+    $(`#day0-UV`).css( "background-color", varUV);
+    for (var i = 0; i <= 5; i++) {
+        $(`#day`+i+`-temp`).text(weather[i].temp);
+        $(`#day`+i+`-icon`).html(weather[i].icon);
+        $(`#day`+i+`-hum`).text(weather[i].hum);
+        $(`#day`+i+`-date`).html(weather[i].date);
+        $(`#day`+i+`-icon`).attr("src",weather[i].icon);
+    }
+}
