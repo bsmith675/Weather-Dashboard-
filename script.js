09db82236adb5a064b720d46034e068339b1d2f3
Ben Smith <benlsmith329@gmail.com>
2022-04-12 20:42:42 -0400

    added variables and query selectors

---
+++ script.js
@@ -0,0 +1,6 @@
+var searchFormEl = document.querySelector('#search-city');
+var resultTextEl = document.querySelector('#currentCityName');
+var currentCityName;
+var APIkey = '23b89ccd37ecfa0524d0c35eae3690f8';
+var weather = [];
+var cityList = [];
